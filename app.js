18b2de2899ff9a65c0bac59fad200bccbe785d69
Jordy <jordynavalee@gmail.com>
2022-08-10 16:03:37 -0500

    got past the freakin' css page

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-const { application } = require("express");
+const express = require("express");
+const app = express();
+const path = require('path');
+// const router = express.Router();
+
+
+const { main } = require("./views");
+
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
+app.use(express.urlencoded({ extended: false }));
+const staticMiddleware = express.static(path.join(__dirname, 'public'));
+app.use(staticMiddleware);
 
 app.get("/", (req, res) => {
-    res.send(console.log('hello world'));
+    res.send(main('')); 
 })
+
+let PORT = 3000;
+
+app.listen(PORT, ()=> console.log(`listening on PORT ${PORT}`));
